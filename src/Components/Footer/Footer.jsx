5e8b8574cd4e615cf3d3d606c9a25091a76bfc99
Mohammad Hosein <en.mohammadhosein@gmail.com>
2023-08-06 23:19:15 +0330

    refactor: footer and socials

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,24 +1,42 @@
-import React from 'react'
-import './Footer.css'
-import { BsGithub, BsInstagram, BsTwitter } from 'react-icons/bs'
-import { AiOutlinePlayCircle, AiOutlineYoutube } from 'react-icons/ai'
+import "./Footer.css";
+import { BsGithub, BsInstagram, BsTwitter } from "react-icons/bs";
+import { AiOutlinePlayCircle } from "react-icons/ai";
+import {FiYoutube} from 'react-icons/fi'
+import { Col, Container, Row } from "react-bootstrap";
+import SocialIcon from "../SocialIcon/SocialIcon";
 export default function Footer() {
-    return (
-        <div className='Container'>
-            <div className="SocialIcons">
-                <BsGithub className='Icons' />
-                <BsInstagram className='Icons' />
-                <BsTwitter className='Icons' />
-                <AiOutlineYoutube className='Icons' />
-            </div>
-            <div className="Watch">
-                <AiOutlinePlayCircle className='Icons' />
-                <span style={{ cursor: "pointer" }}> whatch video </span >
-            </div>
-            <div className="Number">
-                <h3>01</h3>
-                <span>/03</span>
-            </div>
-        </div>
-    )
+  return (
+    <Container>
+      <Row className="align-items-center">
+        <Col xs={12} md={4} className="d-flex align-items-center justify-content-start">
+          <div className="SocialIcons texxt-center">
+            <SocialIcon link="#">
+              <BsGithub className="text-light fs-4" />
+            </SocialIcon>
+            <SocialIcon link="#">
+              <BsInstagram className="text-light fs-4" />
+            </SocialIcon>
+            <SocialIcon link="#">
+              <BsTwitter className="text-light fs-4" />
+            </SocialIcon>
+            <SocialIcon link="#">
+              <FiYoutube className="text-light fs-4" />
+            </SocialIcon>
+          </div>
+        </Col>
+        <Col xs={12} md={4} className="d-flex justify-content-center align-items-center">
+          <div className="Watch">
+            <AiOutlinePlayCircle className="Icons" />
+            <span style={{ cursor: "pointer" }}> whatch video </span>
+          </div>
+        </Col>
+        <Col xs={12} md={4} className="d-flex justify-content-end align-items-center">
+          <div className="Number">
+            <h3>01</h3>
+            <span>/03</span>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
