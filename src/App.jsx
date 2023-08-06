edba85d2def03ae024a1f92fadf076dfb8e1a19f
erfaneton <erfanshafiei.ea@gmail.com>
2023-08-06 18:12:29 +0330

    feat: navbar section ended

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,10 @@ import Navbar from './Components/Navbar/Navbar'
 
 function App() {
     return (
-        <>
+        <div className='vh-100 bg-success'>
             <Navbar></Navbar>
-        </>
+
+        </div>
     )
 }
 export default App
