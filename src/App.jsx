8ada426568ebf6a21eda53588a7274754fd22a98
erfaneton <erfanshafiei.ea@gmail.com>
2023-08-06 17:46:43 +0330

    feat: Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+
 
 function App() {
-
+    return (
+        <>
+            <Navbar></Navbar>
+        </>
+    )
 }
 export default App
